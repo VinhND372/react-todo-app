dc6d4d25a477d19080324a7a60656cdf19ad828d
VinhND372 <72238327+VinhND372@users.noreply.github.com>
2022-09-26 17:41:26 +0700

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from '../styles/modules/button.module.scss';
 import { getClasses } from '../utils/getClasses';
 
+
 const buttonTypes = {
   primary: 'primary',
   secondary: 'secondary',
